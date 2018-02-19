f99ef7e4d306d100dd6fd6fc85016beccdd5ddb7
imcquee <imcquee@clemson.edu>
2018-02-19 12:34:19 -0500

    Update setup-controller.sh

--- setup-controller.sh
+++ setup-controller.sh
@@ -4460,7 +4460,7 @@ openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-
 openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.24 testport4
 
 #####################################################################################################################
-wget -O /tmp/setup/OL7.vmdk https://clemson.box.com/shared/static/u5eno9ft3o04w1323z5udgd7rwj7kncd.vmdk
+wget -O /tmp/setup/OL7.vmdk https://clemson.box.com/shared/static/5ss7qq2bc424h0hrfph37daqjmra1mlu.vmdk
 
 glance image-create --name OL7 --disk-format vmdk --visibility public --container-format bare < /tmp/setup/OL7.vmdk
 
